4660e92bb3b338b8f1a32f6f631cffd995979fda
Stephan Osthold <osthold@modell-aachen.de>
2012-12-12 15:04:14 +0100

    Release 1.5.29

--- lib/Foswiki/Plugins/KVPPlugin.pm
+++ lib/Foswiki/Plugins/KVPPlugin.pm
@@ -20,7 +20,7 @@ use Foswiki::OopsException ();
 use Foswiki::Sandbox ();
 
 our $VERSION          = '$Rev: 7808 (2010-06-15) $';
-our $RELEASE          = '1.5.28';
+our $RELEASE          = '1.5.29';
 our $SHORTDESCRIPTION = 'Kontinuierliche Verbesserung im Wiki';
 our $NO_PREFS_IN_TOPIC = 1;
 our $pluginName       = 'KVPPlugin';
