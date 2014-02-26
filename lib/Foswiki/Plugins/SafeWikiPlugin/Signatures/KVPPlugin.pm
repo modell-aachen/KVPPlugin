711d2a903c1b9aaa8b5ee38c626116a6747d5a3b
Stephan Osthold <osthold@modell-aachen.de>
2014-02-26 13:30:52 +0100

    Update signatures for language fix
    Ticket: #3159

--- lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/KVPPlugin.pm
+++ lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/KVPPlugin.pm
@@ -1,7 +1,7 @@
 # This contains signatures for inline JS code contained in KVPPlugin.
 [
     'KNXp6pTRT+kfJ8uqaaquQxYaFg+7w6ZJL9mceGz8JxE', # KVPPlugin: "Change status" button
-    'e6U2nDYG5s1r7YKcZdOYzoPkAa5cp9WPWkYbxUxDHPs', # solrHitTemplate_topic XXX these have to be signed again when ModacSkin changes
+    'lPQxoRKoa6D9BE+26jkwZJ8abiGtzZi8vYVmEHWuReI', # solrHitTemplate_topic XXX these have to be signed again when ModacSkin changes
     'WJV/cTrkYIxzgWwbNkbSVho1VKZ0Upvc52bod6IFQl4', # solrHitTemplate_image
     'krzjDoX9FGoxtbMAkLfuGMvHvLfYivPtik7I3lYxcc8', # solrHitTemplate_comment
     'Dn3YCmxJF78wllIIsGkiXU9JV+s1ZTo1R8SsQEWxW30', # solrHitTemplate_misc
