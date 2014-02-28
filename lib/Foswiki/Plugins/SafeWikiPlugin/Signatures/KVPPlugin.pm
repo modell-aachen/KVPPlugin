1368ac1eb085b1f2234cf516a26cdadc79180e1d
Stephan Osthold <osthold@modell-aachen.de>
2014-02-28 08:38:41 +0100

    Updating signatures for SafeWikiPlugin (solr::thumbnails)
    %TMPL:DEF{"solr::thumbnails"}% has changed.
    
    Ticket: #3159

--- lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/KVPPlugin.pm
+++ lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/KVPPlugin.pm
@@ -1,7 +1,7 @@
 # This contains signatures for inline JS code contained in KVPPlugin.
 [
     'KNXp6pTRT+kfJ8uqaaquQxYaFg+7w6ZJL9mceGz8JxE', # KVPPlugin: "Change status" button
-    'lPQxoRKoa6D9BE+26jkwZJ8abiGtzZi8vYVmEHWuReI', # solrHitTemplate_topic XXX these have to be signed again when ModacSkin changes
+    'hEumd4rWF2jeeTZpx+VzjYi7GIeSpiF797tRb99U4j0', # solrHitTemplate_topic XXX these have to be signed again when ModacSkin changes
     'WJV/cTrkYIxzgWwbNkbSVho1VKZ0Upvc52bod6IFQl4', # solrHitTemplate_image
     'krzjDoX9FGoxtbMAkLfuGMvHvLfYivPtik7I3lYxcc8', # solrHitTemplate_comment
     'Dn3YCmxJF78wllIIsGkiXU9JV+s1ZTo1R8SsQEWxW30', # solrHitTemplate_misc
