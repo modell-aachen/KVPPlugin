2c4fa986c55bc182fcdc196ff8aaf2967be7d25e
Stephan Osthold <osthold@modell-aachen.de>
2014-02-28 09:37:17 +0100

    Repairing solr::thumbnail
    Ticket: #3159

--- lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/KVPPlugin.pm
+++ lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/KVPPlugin.pm
@@ -1,8 +1,7 @@
 # This contains signatures for inline JS code contained in KVPPlugin.
 [
     'KNXp6pTRT+kfJ8uqaaquQxYaFg+7w6ZJL9mceGz8JxE', # KVPPlugin: "Change status" button
-    'hEumd4rWF2jeeTZpx+VzjYi7GIeSpiF797tRb99U4j0', # solrHitTemplate_topic XXX these have to be signed again when ModacSkin changes
+    'Yek1smoKxdkVM723E3YrhlpSN93Qkxz26aNyp80EGek', # solrHitTemplate_topic XXX these have to be signed again when ModacSkin changes
     'WJV/cTrkYIxzgWwbNkbSVho1VKZ0Upvc52bod6IFQl4', # solrHitTemplate_image
-    'krzjDoX9FGoxtbMAkLfuGMvHvLfYivPtik7I3lYxcc8', # solrHitTemplate_comment
     'Dn3YCmxJF78wllIIsGkiXU9JV+s1ZTo1R8SsQEWxW30', # solrHitTemplate_misc
 ];
