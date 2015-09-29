62e50551aa0496c25bbf31494e926e18e903559a
Stephan Osthold <osthold@modell-aachen.de>
2015-09-29 11:55:19 +0200

    Actually adding (German) translations to solr page
    Ticket: none
    Release-Note(user, change) {
      Adding further translations to the solr page.
    }

---
+++ pub/System/KVPPlugin/jsi18n.solrkvp.de.js
@@ -0,0 +1,13 @@
+jsi18n.add('solr', {
+    "not approved": "nicht freigegeben",
+    "state not defined": "Status nicht definiert",
+    "APPROVED": "Freigegeben",
+    "DRAFT": "Entwurf",
+    "NEW": "Neu",
+    "DISCUSSION": "Diskussionsstand",
+    "DISCARDED": "Verworfen",
+    "FORMAL_REVIEW": "Formale Prüfung",
+    "FORMAL_REVIEW_DRAFT": "Formale Prüfung (Entwurf)",
+    "CONTENT_REVIEW": "Inhaltliche Prüfung",
+    "CONTENT_REVIEW_DRAFT": "Inhaltliche Prüfung (Entwurf)"
+});
---
+++ pub/System/KVPPlugin/jsi18n.solrkvp.en.js
@@ -0,0 +1,11 @@
+jsi18n.add('solr', {
+    "APPROVED": "approved",
+    "DRAFT": "draft",
+    "NEW": "new",
+    "DISCUSSION": "discussion",
+    "DISCARDED": "discarded",
+    "FORMAL_REVIEW": "formal review",
+    "FORMAL_REVIEW_DRAFT": "formal review (draft)",
+    "CONTENT_REVIEW": "content review",
+    "CONTENT_REVIEW_DRAFT": "content review (draft)"
+});
