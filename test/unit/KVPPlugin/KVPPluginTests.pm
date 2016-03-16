444d50032aa433e82fefbc820b37b355e59ff682
Stephan Osthold <osthold@modell-aachen.de>
2016-03-16 11:16:47 +0100

    SeleniumTests: Fix test_suffixTests
    It seems the 'view' context is no loger set automatically.
    
    Ticket: none
    Release-Note: none

--- test/unit/KVPPlugin/KVPPluginTests.pm
+++ test/unit/KVPPlugin/KVPPluginTests.pm
@@ -74,21 +74,21 @@ sub test_suffixTests {
 
     # Fork with default-Suffix
     Helper::createWithState( $this, Helper::KVPWEB, 'SuffixTest', 'FREIGEGEBEN' );
-    $this->createNewFoswikiSession( $user, $query );
+    $this->createNewFoswikiSession( $user, $query, {view => 1} );
     $this->assert( !Foswiki::Func::expandCommonVariables("%IF{\"context KVPHasDiscussion\" then=\"1\" else=\"0\"}%"), "test-topic SuffixTest already has a discussion!" );
     Helper::createDiscussion( $this, Helper::KVPWEB, 'SuffixTest' );
     $this->assert(Foswiki::Func::topicExists( Helper::KVPWEB, 'SuffixTestTALK' ), "Could not find discussion with default suffix!");
-    $this->createNewFoswikiSession( $user, $query );
+    $this->createNewFoswikiSession( $user, $query, {view => 1} );
     $this->assert( Foswiki::Func::expandCommonVariables("%IF{\"context KVPHasDiscussion\" then=\"1\" else=\"0\"}%"), "Discussion with defaultsuffix does not set context!" );
 
     try {
         # Now fork with different suffix
         $Foswiki::cfg{Extensions}{KVPPlugin}{suffix} = 'AndNowForSomethingCompletelyDifferent';
-        $this->createNewFoswikiSession( $user, $query );
+        $this->createNewFoswikiSession( $user, $query, {view => 1} );
         $this->assert( !Foswiki::Func::expandCommonVariables("%IF{\"context KVPHasDiscussion\" then=\"1\" else=\"0\"}%"), "Context still reports discussion with default-suffix altough suffix changed!" );
         Helper::createDiscussion( $this, Helper::KVPWEB, 'SuffixTest' );
         $this->assert(Foswiki::Func::topicExists( Helper::KVPWEB, 'SuffixTestAndNowForSomethingCompletelyDifferent' ), "Could not find discussion with changed suffix!");
-        $this->createNewFoswikiSession( $user, $query );
+        $this->createNewFoswikiSession( $user, $query, {view => 1} );
         $this->assert( Foswiki::Func::expandCommonVariables("%IF{\"context KVPHasDiscussion\" then=\"1\" else=\"0\"}%"), "Context still does not report discussion with changed suffix!" );
     } finally {
         $Foswiki::cfg{Extensions}{KVPPlugin}{suffix} = $defaultSuffix;
